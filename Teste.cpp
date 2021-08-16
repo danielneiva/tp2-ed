e6e20885b9c04c1100acdb1c217a1e9ea0ab373e
Daniel Neiva <neiva.daniel@outlook.com>
2021-08-15 22:58:28 -0300

    Merge Sort Implemented

---
+++ Teste.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "Conscience.h"
+using namespace std;
+
+/**
+ * Merges two portions of 'cons'
+ * 
+ * The first array is the leftmost part of the array
+ * being merged
+ * 
+ * 
+ * The second array is the rightmost part of the array
+ * being merged
+ * 
+ */
+
+void merge(Conscience *cons, int const left, int const mid, int const right)
+{
+    auto const leftPortionSize = mid - left + 1;
+    auto const rightPortionSize = right - mid;
+ 
+    // Create temporary arrays to store the values of the portions
+    auto *leftArray = new Conscience[leftPortionSize],
+         *rightArray = new Conscience[rightPortionSize];
+ 
+    // Gets the data from cons and insert it into leftArray and roghtArray
+    for (auto i = 0; i < leftPortionSize; i++)
+        leftArray[i].name_person = cons[left + i].name_person;
+    for (auto j = 0; j < rightPortionSize; j++)
+        rightArray[j].name_person = cons[mid + 1 + j].name_person;
+ 
+    auto indexOfleftPortionSize = 0, // Initial index of first portion
+        indexOfrightPortionSize = 0; // Initial index of second portion
+    int indexOfMergedArray = left; // Initial index of merged array
+ 
+    // Merge the temporary arrays back into cons
+    while (indexOfleftPortionSize < leftPortionSize && indexOfrightPortionSize < rightPortionSize) {
+        if (leftArray[indexOfleftPortionSize].name_person <= rightArray[indexOfrightPortionSize].name_person) {
+            cons[indexOfMergedArray].name_person = leftArray[indexOfleftPortionSize].name_person;
+            indexOfleftPortionSize++;
+        }
+        else {
+            cons[indexOfMergedArray].name_person = rightArray[indexOfrightPortionSize].name_person;
+            indexOfrightPortionSize++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfleftPortionSize < leftPortionSize) {
+        cons[indexOfMergedArray].name_person = leftArray[indexOfleftPortionSize].name_person;
+        indexOfleftPortionSize++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfrightPortionSize < rightPortionSize) {
+        cons[indexOfMergedArray].name_person = rightArray[indexOfrightPortionSize].name_person;
+        indexOfrightPortionSize++;
+        indexOfMergedArray++;
+    }
+}
+ 
+/**
+ *  Begin is the first potition of the array,
+ *  End is the last position of the array 
+ * 
+ */
+void mergeSort(Conscience *cons, int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+ 
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(cons, begin, mid);
+    mergeSort(cons, mid + 1, end);
+    merge(cons, begin, mid, end);
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <chrono>
+#include <stdlib.h>
 
 struct Conscience{
   std::string name_person ="";
@@ -38,8 +39,75 @@ namespace QuickS{
   }
 }
 
+namespace MergeS{
+  void merge(Conscience *cons, int const left, int const mid, int const right){
+    auto const leftPortionSize = mid - left + 1;
+    auto const rightPortionSize = right - mid;
+
+    // Create temporary arrays to store the values of the portions
+    auto *leftArray = new Conscience[leftPortionSize];
+    auto *rightArray = new Conscience[rightPortionSize];
+
+    // Gets the data from cons and insert it into leftArray and roghtArray
+    for (auto i = 0; i < leftPortionSize; i++){
+      leftArray[i].name_person = cons[left + i].name_person;
+    }
+    for (auto j = 0; j < rightPortionSize; j++){
+      rightArray[j].name_person = cons[mid + 1 + j].name_person;
+    }
+
+    auto indexOfleftPortionSize = 0;  // Initial index of first portion
+    auto indexOfrightPortionSize = 0; // Initial index of second portion
+    int indexOfMergedArray = left;    // Initial index of merged array
+
+    // Merge the temporary arrays back into cons
+    while (indexOfleftPortionSize < leftPortionSize && indexOfrightPortionSize < rightPortionSize) {
+      if (leftArray[indexOfleftPortionSize].name_person <= rightArray[indexOfrightPortionSize].name_person) {
+        cons[indexOfMergedArray].name_person = leftArray[indexOfleftPortionSize].name_person;
+        indexOfleftPortionSize++;
+      }
+      else {
+        cons[indexOfMergedArray].name_person = rightArray[indexOfrightPortionSize].name_person;
+        indexOfrightPortionSize++;
+      }
+      indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfleftPortionSize < leftPortionSize) {
+      cons[indexOfMergedArray].name_person = leftArray[indexOfleftPortionSize].name_person;
+      indexOfleftPortionSize++;
+      indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfrightPortionSize < rightPortionSize) {
+      cons[indexOfMergedArray].name_person = rightArray[indexOfrightPortionSize].name_person;
+      indexOfrightPortionSize++;
+      indexOfMergedArray++;
+    }
+  }
+  
+  /**
+   *  Begin is the first potition of the array,
+   *  End is the last position of the array 
+   * 
+   */
+  void mergeSort(Conscience *cons, int const begin, int const end)
+  {
+    if (begin >= end)
+      return;
+
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(cons, begin, mid);
+    mergeSort(cons, mid + 1, end);
+    merge(cons, begin, mid, end);
+  }
+
+}
+
 void loadConsciences(Conscience *cons, std::string file, int numCons){
-  std::ifstream infile("homologacao.txt");
+  std::ifstream infile(file);
   int numLine=0;
   std::string line;
   while (std::getline(infile, line) && numLine < numCons){
@@ -50,10 +118,7 @@ void loadConsciences(Conscience *cons, std::string file, int numCons){
     cons[numLine].conscience_person = consLine;
     numLine++;
   } 
-}
-
-namespace MergeS{
-
+  std::cout << "Consciences loaded into the system!\nStarting audits...\n";
 }
 
 int main(int argc, char **argv) {
@@ -63,28 +128,31 @@ int main(int argc, char **argv) {
      * 
      * 
     */ 
-    std::istringstream fileNameStream(argv[1]);
-    std::string fileName;
-    fileNameStream >> fileName;
-    std::istringstream testTypeStream(argv[2]);
-    int testType;
-    testTypeStream >> testType;
-    std::istringstream numConsStream(argv[3]);
-    int numCons;
-    numConsStream >> numCons;
+    std::string fileName = argv[1];
+    int testType = strtol(argv[2], nullptr, 0);
+    int numCons = strtol(argv[3], nullptr, 0);;
+    std::cout << numCons << " - " << fileName << " - " << testType << std::endl;
+    Conscience *rellocatorCO = new Conscience[numCons];
+    loadConsciences(rellocatorCO, fileName, numCons);
 
-    Conscience *rellocatorCO = new Conscience[200000];
-    loadConsciences(rellocatorCO, "", numCons);
-
+    
+/* 
+    // QUICKSORT
     auto start = std::chrono::high_resolution_clock::now();
     QuickS::quickSort(rellocatorCO, 0, numCons -1 );
     auto end = std::chrono::high_resolution_clock::now();
+ */
+    // MERGESORT
+    auto start = std::chrono::high_resolution_clock::now();
+    MergeS::mergeSort(rellocatorCO, 0, numCons -1 );
+    auto end = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start );
+    auto durationMS = std::chrono::duration_cast<std::chrono::microseconds>(end - start );
     for (int i =0; i<numCons;i++){
       std::cout << "nome: " << rellocatorCO[i].name_person << std::endl;
     }
-    std::cout << duration.count() << std::endl;
+
+    std::cout << "It took: " << durationMS.count() << " Microseconds" << std::endl;
     delete [] rellocatorCO;
   return 0;
 }
