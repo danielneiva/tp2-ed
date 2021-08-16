3932d76bd67c864caa0e9d8be618a655a420e14d
Daniel Neiva <neiva.daniel@outlook.com>
2021-08-15 23:37:00 -0300

    Heap Sort Implemented

--- Teste.cpp
+++ Teste.cpp
@@ -1,78 +1,74 @@
 #include <iostream>
 #include "Conscience.h"
 using namespace std;
-
-/**
- * Merges two portions of 'cons'
- * 
- * The first array is the leftmost part of the array
- * being merged
- * 
- * 
- * The second array is the rightmost part of the array
- * being merged
- * 
- */
-
-void merge(Conscience *cons, int const left, int const mid, int const right)
+ 
+// A utility function to get maximum value in arr[]
+int getMax(Conscience *cons, int n)
 {
-    auto const leftPortionSize = mid - left + 1;
-    auto const rightPortionSize = right - mid;
- 
-    // Create temporary arrays to store the values of the portions
-    auto *leftArray = new Conscience[leftPortionSize],
-         *rightArray = new Conscience[rightPortionSize];
- 
-    // Gets the data from cons and insert it into leftArray and roghtArray
-    for (auto i = 0; i < leftPortionSize; i++)
-        leftArray[i].name_person = cons[left + i].name_person;
-    for (auto j = 0; j < rightPortionSize; j++)
-        rightArray[j].name_person = cons[mid + 1 + j].name_person;
- 
-    auto indexOfleftPortionSize = 0, // Initial index of first portion
-        indexOfrightPortionSize = 0; // Initial index of second portion
-    int indexOfMergedArray = left; // Initial index of merged array
- 
-    // Merge the temporary arrays back into cons
-    while (indexOfleftPortionSize < leftPortionSize && indexOfrightPortionSize < rightPortionSize) {
-        if (leftArray[indexOfleftPortionSize].name_person <= rightArray[indexOfrightPortionSize].name_person) {
-            cons[indexOfMergedArray].name_person = leftArray[indexOfleftPortionSize].name_person;
-            indexOfleftPortionSize++;
-        }
-        else {
-            cons[indexOfMergedArray].name_person = rightArray[indexOfrightPortionSize].name_person;
-            indexOfrightPortionSize++;
-        }
-        indexOfMergedArray++;
-    }
-    // Copy the remaining elements of
-    // left[], if there are any
-    while (indexOfleftPortionSize < leftPortionSize) {
-        cons[indexOfMergedArray].name_person = leftArray[indexOfleftPortionSize].name_person;
-        indexOfleftPortionSize++;
-        indexOfMergedArray++;
-    }
-    // Copy the remaining elements of
-    // right[], if there are any
-    while (indexOfrightPortionSize < rightPortionSize) {
-        cons[indexOfMergedArray].name_person = rightArray[indexOfrightPortionSize].name_person;
-        indexOfrightPortionSize++;
-        indexOfMergedArray++;
-    }
+    Conscience mx = cons[0];
+    for (int i = 1; i < n; i++)
+        if (cons[i].conscience_person > mx.conscience_person)
+            mx.conscience_person = cons[i].conscience_person;
+    return mx.conscience_person;
 }
  
-/**
- *  Begin is the first potition of the array,
- *  End is the last position of the array 
- * 
- */
-void mergeSort(Conscience *cons, int const begin, int const end)
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp)
 {
-    if (begin >= end)
-        return;
+    int output[n]; // output array
+    int i, count[10] = { 0 };
  
-    auto mid = begin + (end - begin) / 2;
-    mergeSort(cons, begin, mid);
-    mergeSort(cons, mid + 1, end);
-    merge(cons, begin, mid, end);
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+ 
+    // Change count[i] so that count[i] now contains actual
+    //  position of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+ 
+    // Build the output array
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+ 
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to current digit
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+ 
+// The main function to that sorts arr[] of size n using
+// Radix Sort
+void radixsort(int arr[], int n)
+{
+    // Find the maximum number to know number of digits
+    int m = getMax(arr, n);
+ 
+    // Do counting sort for every digit. Note that instead
+    // of passing digit number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+ 
+// A utility function to print an array
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+ 
+// Driver Code
+int main()
+{
+    int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+     
+      // Function Call
+      radixsort(arr, n);
+    print(arr, n);
+    return 0;
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -106,6 +106,59 @@ namespace MergeS{
 
 }
 
+namespace HeapS{
+  // To heapify a subtree rooted with node i which is
+  // an index in arr[]. n is size of heap
+  void swap ( Conscience *a, Conscience *b ){
+    Conscience aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+  void heapify(Conscience *cons, int heapSize, int index)
+  {
+    int largest = index; // Initialize largest as root
+    int left = 2 * index + 1; // left = 2*i + 1
+    int right = 2 * index + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (left < heapSize && cons[left].conscience_person > cons[largest].conscience_person){
+      largest = left;
+    }
+
+    // If right child is larger than largest so far
+    if (right < heapSize && cons[right].conscience_person > cons[largest].conscience_person){
+      largest = right;
+    }
+
+    // If largest is not root
+    if (largest != index) {
+      swap(&cons[index], &cons[largest]);
+
+      // Recursively heapify the affected sub-tree
+      heapify(cons, heapSize, largest);
+    }
+  }
+  
+  // main function to do heap sort
+  void heapSort(Conscience *cons, int numCons)
+  {
+      // Build heap (rearrange array)
+      for (int i = numCons / 2 - 1; i >= 0; i--){
+          heapify(cons, numCons, i);
+      }
+  
+      // One by one extract an element from heap
+      for (int i = numCons - 1; i > 0; i--) {
+          // Move current root to end
+          swap(&cons[0], &cons[i]);
+  
+          // call max heapify on the reduced heap
+          heapify(cons, i, 0);
+      }
+  }
+}
+
 void loadConsciences(Conscience *cons, std::string file, int numCons){
   std::ifstream infile(file);
   int numLine=0;
@@ -142,14 +195,19 @@ int main(int argc, char **argv) {
     QuickS::quickSort(rellocatorCO, 0, numCons -1 );
     auto end = std::chrono::high_resolution_clock::now();
  */
+/* 
     // MERGESORT
     auto start = std::chrono::high_resolution_clock::now();
     MergeS::mergeSort(rellocatorCO, 0, numCons -1 );
     auto end = std::chrono::high_resolution_clock::now();
+ */
+    auto start = std::chrono::high_resolution_clock::now();
+    HeapS::heapSort(rellocatorCO, numCons  );
+    auto end = std::chrono::high_resolution_clock::now();
 
     auto durationMS = std::chrono::duration_cast<std::chrono::microseconds>(end - start );
     for (int i =0; i<numCons;i++){
-      std::cout << "nome: " << rellocatorCO[i].name_person << std::endl;
+      std::cout << "conscience: " << rellocatorCO[i].conscience_person << " - meaning: " << stoi(rellocatorCO[i].conscience_person, 0, 2) << std::endl;
     }
 
     std::cout << "It took: " << durationMS.count() << " Microseconds" << std::endl;
